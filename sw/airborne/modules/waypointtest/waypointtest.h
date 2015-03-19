64de99e93f73ffcf9da5f5ded2d6431e590bd12f
Niek Klein Koerkamp <niek@ubuntu.(none)>
2015-03-19 16:01:21 +0100

    Initial waypointtest module + flight plan implementation. Not yet working but compiles fine.

---
+++ sw/airborne/modules/waypointtest/waypointtest.h
@@ -0,0 +1,7 @@
+#include "firmwares/rotorcraft/navigation.h"
+#include "generated/flight_plan.h"
+#include "math/pprz_geodetic_int.h"
+
+void waypointtest_init(void);
+
+void waypointtest_periodic(void);
