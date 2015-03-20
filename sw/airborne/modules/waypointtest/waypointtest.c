730397db8f14592a9d52b439b02ff93946c929ce
Niek Klein Koerkamp <niek@ubuntu.(none)>
2015-03-20 11:19:40 +0100

    Airframe files for ardrone now include waypointtest module and have limited pitch and bank angles for lower maneuver speed

--- sw/airborne/modules/waypointtest/waypointtest.c
+++ sw/airborne/modules/waypointtest/waypointtest.c
@@ -21,17 +21,20 @@ void waypointtest_init(void)
 void waypointtest_periodic(void)
 {
 	struct LlaCoor_i waypoint;
-	waypoint.lat=51.990680;
-	waypoint.lon=4.376790;
+	waypoint.lat=51.9906689;
+	waypoint.lon=4.3768509;
 	waypoint.alt=200;
 
-	unsigned int i=0;
+	nav_move_waypoint_lla(WP_DynamicWP, &waypoint);
+	nav_set_heading_towards_waypoint(WP_DynamicWP);
+
+	/*unsigned int i=0;
    	
 	for(i=0; i<60000; i++) {	
 		waypoint.lat=waypoint.lat+0.000002;
 		waypoint.lon=waypoint.lon+0.000002;
 		nav_move_waypoint_lla(WP_DynamicWP, &waypoint);
 		nav_set_heading_towards_waypoint(WP_DynamicWP);
-  	}
+  	}*/
 }
 
